3f6de639da3c8f244930869cf9e1e22ce6bfdf69
tomk132 <kucinskitomasz@gmail.com>
2026-01-05 17:46:50 +0100

    Wykonana zejściówka

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -1,8 +1,46 @@
 #pragma once
 
 #include "Resource.hpp"
+#include <iostream>
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+
+public:
+    ResourceManager() : res(new Resource) {};
+    ~ResourceManager() { delete res; };
+    ResourceManager(const ResourceManager& other) : res{new Resource(*other.res)} {};
+    ResourceManager& operator=(const ResourceManager& other)
+    {
+
+        if (this == &other)
+            return *this;
+
+        Resource* nowy = new Resource{*other.res};
+        delete this->res;
+        this->res = nowy;
+
+        return *this;
+    };
+    ResourceManager(ResourceManager&& other) noexcept : res{other.res} { other.res = nullptr; };
+
+    ResourceManager& operator=(ResourceManager&& other) noexcept
+    {
+
+        if (this == &other) {
+            return *this;
+        };
+
+        delete this->res;
+        this->res = nullptr; 
+        this->res = other.res; 
+        other.res = nullptr;
+
+        return *this;
+    };
+
+    double get() { return res->get(); };
+
+private:
+    Resource* res;
 };
